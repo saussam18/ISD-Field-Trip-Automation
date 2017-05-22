978eb83d538d54b3fe36e8fd57518ff3d4b486e1
saussam18 <sammysau@hotmail.com>
2017-05-21 21:42:11 -0700

    Add files via upload

---
+++ connectivity.php
@@ -0,0 +1,22 @@
+<?php
+define('HOST', 'localhost');//defines host varible, will need to change to server to implement
+define('NAME', 'practice');//finds table sql in code
+ define('USER','root');//user name to access database, default is always root
+ define('PASSWORD','');//password to access user name, default is always blank
+ $connect = mysql_connect(HOST,USER,PASSWORD) or die("Failed to connect to MySQL: " . mysql_error()); //checks that it can connect to server
+ $find = mysql_select_db(NAME,$connect) or die("Failed to find to MySQL Server:" . mysql_error()); //checks if database exists
+  function SignIn() { session_start();//session start, only done if submit button is pressed
+     if(!empty($_POST['user']))  { //checks if username and password has text and if it exists
+       $query = mysql_query("SELECT * FROM UserName where userName = '$_POST[user]' AND pass = '$_POST[pass]'") or die(mysql_error()); //checks if user and password is avaible
+        $row = mysql_fetch_array($query); // fetches data
+         if(!empty($row['userName']) AND !empty($row['pass'])) {//checks if user and password is correct
+           $_SESSION['userName'] = $row['pass'];
+            echo "SUCCESSFULLY LOGIN TO USER PROFILE PAGE..."; //success
+           } else {
+             echo "SORRY... YOU ENTERD WRONG ID AND PASSWORD... PLEASE RETRY..."; //fail
+           }
+         }
+       } if(isset($_POST['submit'])) { //button that runs the function
+         SignIn();
+       }
+       ?>
---
+++ data.sql
@@ -0,0 +1,10 @@
+CREATE TABLE UserName ( UserNameID int(9) NOT NULL auto_increment,
+userName VARCHAR(40) NOT NULL,
+pass VARCHAR(40) NOT NULL,
+PRIMARY KEY(UserNameID)
+);
+
+INSERT INTO
+UserName (userName, pass)
+VALUES
+("saussam18","0864196");
