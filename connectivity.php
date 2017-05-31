d28426c31ad620f9c3c97a65581fab7bb296f4a7
Ginsu Eddy <ginsu.eddy@gmail.com>
2017-05-31 09:29:38 -0700

    Made code easy to read

--- connectivity.php
+++ connectivity.php
@@ -1,40 +1,41 @@
 <?php
-define('HOST', 'localhost');//defines host varible, will need to change to server to implement
-define('NAME', 'practice');//finds table sql in code
-define('USER','root');//user name to access database, default is always root
-define('PASSWORD','');//password to access user name, default is always blank
+  define('HOST', 'localhost');//defines host varible, will need to change to server to implement
+  define('NAME', 'practice');//finds table sql in code
+  define('USER','root');//user name to access database, default is always root
+  define('PASSWORD','');//password to access user name, default is always blank
 
-$connect = mysql_connect(HOST,USER,PASSWORD) or die("Failed to connect to MySQL: " . mysql_error()); //checks that it can connect to server
-$find = mysql_select_db(NAME,$connect) or die("Failed to find to MySQL Server:" . mysql_error()); //checks if database exists
+  $connect = mysql_connect(HOST,USER,PASSWORD) or die("Failed to connect to MySQL: " . mysql_error()); //checks that it can connect to server
+  $find = mysql_select_db(NAME,$connect) or die("Failed to find to MySQL Server:" . mysql_error()); //checks if database exists
 
-function SignIn() {
-    session_start();//session start, only done if submit button is pressed
+  function SignIn() {
+      session_start();//session start, only done if submit button is pressed
 
-    if(!empty($_POST['Username']) && !empty($_POST['Password']))  { //checks if username and password has text and if it exists
-        $query = mysql_query("SELECT * FROM UserName where userName = '$_POST[Username]' AND pass = '$_POST[Password]' AND  type = '$_POST'[Student/Guardian Access]") or die(mysql_error()); //checks if user and password is avaible
+      if(!empty($_POST['Username']) && !empty($_POST['Password']))  { //checks if username and password has text and if it exists
+          $query = mysql_query("SELECT * FROM UserName where userName = '$_POST[Username]' AND pass = '$_POST[Password]' AND  type = '$_POST'[Student/Guardian Access]") or die(mysql_error()); //checks if user and password is avaible
 
-    if(!empty($_POST['Username']) && !empty($_POST['Password']))  { //checks if username and password has text and if it exists
-        $query = mysql_query("SELECT * FROM UserName where userName = '$_POST[Username]' AND pass = '$_POST[Password]'") or die(mysql_error()); //checks if user and password is avaible
-        $row = mysql_fetch_array($query); // fetches data
-if $!empty($row['type']){
-  if(!empty($row['userName']) AND !empty($row['pass'])) {//checks if user and password is correct
-      $_SESSION['userName'] = $row['pass'];
-      //echo "SUCCESSFULLY LOGIN TO USER PROFILE PAGE..."; //success
-      header("Location: student-page.html");
-    }
-        else {
-            echo "SORRY... YOU ENTERD WRONG ID AND PASSWORD... PLEASE RETRY..."; //fail
+          if(!empty($_POST['Username']) && !empty($_POST['Password']))  { //checks if username and password has text and if it exists
+              $query = mysql_query("SELECT * FROM UserName where userName = '$_POST[Username]' AND pass = '$_POST[Password]'") or die(mysql_error()); //checks if user and password is avaible
+              $row = mysql_fetch_array($query); // fetches data
+
+              if $!empty($row['type']){
+                  if(!empty($row['userName']) AND !empty($row['pass'])) {//checks if user and password is correct
+                      $_SESSION['userName'] = $row['pass'];
+                      //echo "SUCCESSFULLY LOGIN TO USER PROFILE PAGE..."; //success
+                      header("Location: student-page.html");
+                  }
+                  else {
+                      echo "SORRY... YOU ENTERD WRONG ID AND PASSWORD... PLEASE RETRY..."; //fail
+                  }
               }
-            }
-        else {
-            echo "You forgot your username and/or password numb nuts";
-            }
+              else {
+                  echo "You forgot your username and/or password numb nuts";
+              }
           }
-        }
-}
+      }
+  }
 
 
-if(isset($_POST['Submit'])) { //button that runs the function
-    SignIn();
-}
+  if(isset($_POST['Submit'])) { //button that runs the function
+      SignIn();
+  }
 ?>
