2cbb61507b48a3b8492210468ac6682cfcf82fe1
Ginsu Eddy <ginsu.eddy@gmail.com>
2017-06-14 07:24:28 -0700

    bringing variables across using php

--- JS/fill-out-form.js
+++ JS/fill-out-form.js
@@ -1,4 +1,6 @@
-var destination = "test";
+var formInfo = new Array(7);
+
+var destination = "";
 var purpose = "";
 var schoolName = "";
 var startDate = "";
@@ -13,31 +15,45 @@ var waterActivity = false;
 function next() {
     getFieldTripInfo();
     //getDestination();
-    window.location='create-form.html';
+    //alert(destination);
+    //setFieldTripInfo();
+    window.location='create-form.php';
 }
 
 function getFieldTripInfo() {
-  purpose = $('#purp').val();
-  schoolName = $('#school').val();
-  startDate = $('#start_date').val();
-  endDate = $('#end_date').val();
-  startTime = $('#start_time').val();
-  endTime = $('#end_time').val();
+  formInfo[0] = $('#place').val();
+  formInfo[1] = $('#purp').val();
+  formInfo[2] = $('#school').val();
+  formInfo[3] = $('#start_date').val();
+  formInfo[4] = $('#end_date').val();
+  formInfo[5] = $('#start_time').val();
+  formInfo[6] = $('#end_time').val();
+
+  var packed = "";
+  for(var i = 0; i < formInfo.length; i++){
+    if (i > 0) {
+      packed += ",";
+    }
+    packed += escape(formInfo[i]);
+  }
+  document.data.value = packed;
+  document.data.submit();
   //transportation = do later
   //add extended trip and below later
 }
 
 function getDestination(){
-  alert($('#place').val());
-  return $('#place').val();
+  window.localStorage.setItem ('dest', $('#place').val());
+  alert(window.localStorage.getItem('dest'));
 }
+destination = getDestination();
 
-function setFieldTripInfo () {
-  alert(destination);
-  $('#destination_blank').text(destination + "\t");
-  /*$('#purpose_blank').val(purpose);
-  $('#school_blank').val(schoolName);
-  $('#start_date_blank').val() = startDate;
-  $('#start_time_blank').val() = startTime;
-  $('#end_time_blank').val() = endTime;*/
+function setFieldTripInfo (formInfo) {
+  //alert(destination);
+  $('#destination_blank').text(formInfo[0] + "\t");
+  $('#purpose_blank').val(formInfo[1] + "\t");
+  $('#school_blank').val(formInfo[2] + "\t");
+  $('#start_date_blank').val(formInfo[3] + "\t");
+  $('#start_time_blank').val(formInfo[4] + "\t");
+  $('#end_time_blank').val(formInfo[5] + "\t");
 }
