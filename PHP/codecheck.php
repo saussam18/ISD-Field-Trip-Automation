29d1c4c4e6b55446ca3a91ab2e8bf00088e72028
saussam18 <sammysau@hotmail.com>
2017-06-13 23:23:11 -0700

    classes now with student

--- PHP/codecheck.php
+++ PHP/codecheck.php
@@ -1,4 +1,5 @@
 <?php
+require('user.php');
 session_start();//session start, only done if submit button is pressed
 $_SESSION['Error'] = '';
 
@@ -23,10 +24,43 @@ function check(){
   $query = mysql_query("SELECT classcode FROM classes where classcode = '$_POST[code]'") or die(mysql_error()); //checks if user and password is avaible
   $row = mysql_fetch_array($query);
     if($row['classcode'] == $_POST['code']){
-      header("Location: ../HTML/StudentForm.html");
+      $find = mysql_query("SELECT * FROM username");
+      $get = mysql_fetch_array($find);
+      findNull($get);
+      echo "success";
     } else {
       echo "Classcode does not exist";
     }
+}
+function findNull ($get){
+  $find =  mysql_query("SELECT classname FROM classes") or  die(mysql_error());
+  $res =  mysql_fetch_array($find);
+  $per = $_SESSION['user']->getName();
+  $name = $res['classname'];
+    if ($get['class1'] == NULL){
+      $insert = "UPDATE username SET class1 = '$name' WHERE userName = '$per'";
+            $sql = mysql_query($insert) or die(mysql_error());
+  } else if ($get['class2'] == NULL){
+    $insert = "UPDATE username SET class2 = '$name' WHERE userName = '$per'";
+            $sql = mysql_query($insert) or die(mysql_error());
+  }else if ($get['class3'] == NULL){
+    $insert = "UPDATE username SET class3 = '$name' WHERE userName = '$per'";
+            $sql = mysql_query($insert) or die(mysql_error());
+  }else if ($get['class4'] == NULL){
+    $insert = "UPDATE username SET class4 = '$name' WHERE userName = '$per'";
+            $sql = mysql_query($insert) or die(mysql_error());
+  }else if ($get['class5'] == NULL){
+    $insert = "UPDATE username SET class5 = '$name' WHERE userName = '$per'";
+            $sql = mysql_query($insert) or die(mysql_error());
+  }else if ($get['class6'] == NULL){
+    $insert = "UPDATE username SET class6 = '$name' WHERE userName = '$per'";
+            $sql = mysql_query($insert) or die(mysql_error());
+  }else if ($get['class7'] == NULL){
+    $insert = "UPDATE username SET class7 = '$name' WHERE userName = '$per'";
+            $sql = mysql_query($insert) or die(mysql_error());
+  } else {
+    echo "You are out of classes, please delete a class";
+  }
 }
 
 ?>
